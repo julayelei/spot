a443df507a849c94e1f250c17c3762fd51794d25
Julio Rivera <juliorivera.rivas2013@gmail.com>
2017-07-09 15:39:10 -0600

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -1,1 +1,6 @@
-apt-get update && apt-get install lxde && apt-get install lxde-core && apt-get install xorg && apt-get install tightvncserver && reboot
+apt-get update && apt-get install lxde && apt-get install lxde-core && apt-get install xorg && apt-get install tightvncserver && read -p "Esta instancia va a reiniciarse. Presione 'y' para reiniciar..." -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    reboot
+fi
